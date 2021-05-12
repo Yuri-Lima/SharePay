dc3d34dbeb174b72b865147958eafa767dce194b
yurilima <y.m.lima@hotmail.com>
2021-05-12 10:10:56 -0300

    Feat-MainCore_SharePay

---
+++ coresharepay.py
@@ -0,0 +1,23 @@
+"""
+    HouseNameModel
+        meter
+    HouseTenantModel
+        start_date
+        end_date
+        days
+    HouseBillModel
+        start_date_bill
+        end_date_bill
+        days_bill
+    HouseKilowattModel
+        last_read_kwh
+        read_kwh
+        kwh
+"""
+
+
+class CoreSharePay(object):
+    
+
+    def __init__(self):
+        pass
